2eb61c321f64fdb55d80f551ce169997021450c7
zankhana savsani <savsanizankhana123@gmail.com>
2024-08-05 23:27:34 +0530

    done some changes

--- client/src/component/User/Header.jsx
+++ client/src/component/User/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CountryDropdown from "./CountryDropdown";
-import { IoSearch } from "react-icons/io5";
 import { Button } from "@mui/material";
 import { FiUser } from "react-icons/fi";
 import { IoBagOutline } from "react-icons/io5";
+import SearchBox from "./SearchBox";
 
 const Header = () => {
   return (
     <>
       <div className="headerWrapper">
-        <div className="header">
+        <header className="header">
           <div className="container">
             <div className="row">
               <div className="logoWrapper d-flex align-items-center col-sm-2">
@@ -20,14 +20,7 @@ const Header = () => {
               </div>
               <div className="col-sm-10 d-flex align-items-center part2">
                 <CountryDropdown />
-
-                {/* Header Search Start Here */}
-                <div className="headerSearch ml-3 mr-3">
-                  <input type="text" placeholder="Search for products"/>
-                  <Button><IoSearch/></Button>
-                </div>
-                {/* Header Search End Here */}
-
+                <SearchBox />
                 <div className="part3 d-flex align-items-center">
                   <Button className="circle mr-3"><FiUser/></Button>
                   <div className="ml-auto cartTab">
@@ -37,7 +30,21 @@ const Header = () => {
               </div>
             </div>
           </div>
-        </div>
+        </header>
+        <nav>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-3 navPart1">
+                <Button className="allCatTab">
+                  <span className="text">All CATEGORIES</span>
+                </Button>
+              </div>
+              <div className="col-sm-9 navPart2">
+                
+              </div>
+            </div>
+          </div>
+        </nav>
       </div>
     </>
   );
